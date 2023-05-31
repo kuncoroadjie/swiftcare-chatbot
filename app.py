383054c1e8cb374409661ddd1209bc498c2a094c
kuncoroadjie <kuncoroadjie17@gmail.com>
2023-06-01 00:33:45 +0700

    fix routing

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/chatbot")
+def bot():
+    return render_template('chatbot.html')
+
 if __name__ == '__main__':
    app.run()
