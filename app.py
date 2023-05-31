ae3b6671cc1b52c83bfe34abe42fd08ea5ecbb0a
kuncoroadjie <kuncoroadjie17@gmail.com>
2023-06-01 01:08:21 +0700

    add chatbot.py and make chatbot functionality

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import chatbot
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 @app.route("/chatbot")
 def bot():
-    return render_template('chatbot.html')
+    return render_template("chatbot.html")
 
-if __name__ == '__main__':
-   app.run()
+@app.route("/get")
+def chat():
+    userText = request.args.get('msg')
+    return chatbot.chatbot_response(userText)
